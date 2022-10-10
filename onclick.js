70bfa6ee3f0fde90cb6b2a9a9f997a020171ed08
tschelina <113345147+tschelina@users.noreply.github.com>
2022-10-10 13:01:24 +0200

    d

--- onclick.js
+++ onclick.js
@@ -1,10 +1,22 @@
 
 const bezirke = document.getElementsByTagName("g");
 var mistakes = 0;
+const bezirkeArray = [];
+
+function array() {
+    const collection = document.getElementById(this.id).firstElementChild;
+    collection.style.fill = "yellow";
+
+}
 
 
 for (let g of bezirke) {
   g.addEventListener("click", checkMistake);
+  bezirkeArray.push(this.id);
+  const collection = document.getElementById(this.id).firstElementChild;
+    alert(this.id);
+
+
   
   g.addEventListener("mouseover", function(){
     const collection = document.getElementById(this.id).firstElementChild;
@@ -15,15 +27,20 @@ for (let g of bezirke) {
     const collection = document.getElementById(this.id).firstElementChild;
     collection.style.fill = "pink"; 
   });
+
 }
 
 
 
+
+
 function checkMistake() {
-  if (mistakes == 0) {
+  alert("hallo");
+  
+  /*if (mistakes == 0) {
     collection.style.fill = "green";
  }
  if (mistakes == 1) {
 
- }
+ } */
 }
