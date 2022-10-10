af5851e9f6ed8779d5bb104896f5d7cb1c4738e3
Sandy <sandra.schwarz03@gmail.com>
2022-10-10 12:04:02 +0200

    d

--- onclick.js
+++ onclick.js
@@ -1,13 +1,23 @@
 
 const bezirke = document.getElementsByTagName("g");
 
+
+
 for (let g of bezirke) {
   g.addEventListener("click", function(){
     alert(this.id);
   });
   
-  g.addEventListener("click", function(){
+  g.addEventListener("mouseover", function(){
     const collection = document.getElementById(this.id).firstElementChild;
     collection.style.fill = "yellow";
+    
+  });
+
+  g.addEventListener("mouseout", function(){
+    const collection = document.getElementById(this.id).firstElementChild;
+    collection.style.fill = "pink";
+    
   });
 }
+
