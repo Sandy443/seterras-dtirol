9034dbb1e7562a35c547b543d42090002c97a74a
Sandy <sandra.schwarz03@gmail.com>
2022-10-13 12:23:55 +0200

    gh

--- onclick.js
+++ onclick.js
@@ -1,24 +1,12 @@
 
 const bezirke = document.getElementsByTagName("g");
-var mistakes = 0;
-const bezirkeArray = [];
-
-
-  window.onload = function()  {
-    const selection = document.querySelectorAll('g[id]');
-    for (let i = 0; i < selection.length; i++) {
-    bezirkeArray.push(selection[i]);
-  }
-  console.log(bezirkeArray);
-  };
-
 
 for (let g of bezirke) {
 
   g.addEventListener("click", checkMistake);
 
   g.addEventListener("mouseover", function(){
-    const collection = document.getElementById(this.id).firstElementChild; //style ist im path-tag, deshalb child Element
+    const collection = document.getElementById(this.id).firstElementChild;  //style ist im path-tag, deshalb child Element
     collection.style.fill = "yellow";
   });
 
@@ -29,14 +17,6 @@ for (let g of bezirke) {
 
 }
 
-
 function checkMistake() {
   alert(this.id);
-  
-  /*if (mistakes == 0) {
-    collection.style.fill = "green";
- }
- if (mistakes == 1) {
-
- } */
 }
